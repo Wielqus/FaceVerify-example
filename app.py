18481a25ab53d9ac17ec5bb6cad5bbee387be523
Jakub Wielgus <Wielqus@gmail.com>
2021-01-04 01:56:46 +0100

    test

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ def login():
     return render_template('login.html')  
 
 if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
     db.drop_all()
     db.create_all()
     db.session.commit()
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
